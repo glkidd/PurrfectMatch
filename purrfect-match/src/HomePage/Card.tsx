7ea75d904f05ce96ba2165e4250890bba8280e1e
Alona Gorer <alonagorer@Alonas-MacBook-Pro.local>
2019-11-10 20:01:53 -0800

    add .css to non-top-bar-buttons

---
+++ purrfect-match/src/HomePage/Card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Card.css';
+
+interface CardProps {
+  title: string;
+  text?: string;
+  image: string;
+}
+
+const Card = (props: CardProps) => (
+  <div className="Card">
+    <img src={props.image} />
+    <h3>{props.title}</h3>
+    <p>{props.text}</p>
+  </div>
+)
+
+export default Card
