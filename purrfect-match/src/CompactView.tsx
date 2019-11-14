47fec22fd13568b886ea2683143363b9763f0a54
Gwendolyn Kidd <glkidd@calpoly.edu>
2019-11-13 16:44:48 -0800

    fixed indentations

--- purrfect-match/src/CompactView.tsx
+++ purrfect-match/src/CompactView.tsx
@@ -40,23 +40,23 @@ export class CompactView extends React.Component<compactViewProps, compactViewSt
             </button>
 
             <Modal display={this.state.modalOpen} onClose={() => this.setState({ modalOpen: false })}>
-            <img id="successStoryPageImage" src={cat} alt="Cat"/> 
-            <div id="successStoryPageText">
-                <h1 id="successStoryPageHeader"> 
-                    Name: Ollie
-                    <br />Age: 3 years
-                    <br />Breed: American Shorthair
-                    <br />Adopted: 10/24/2019
-                    <br />
-                    <br />
-                    A message from Ollie's owner, Bob:
-                </h1>
-                <p> 
-                    Ollie has been a real friend! Coming home from a crazy day at work, I get to look forward to this goofball greeting me with lots of purring and cuddles. I found Ollie through Purrfect Match. I couldn’t go to my local shelter without first knowing if I had a specific pet in mind, because I can’t be away from work for very long during business hours. Perrfect Match helped me find Ollie, and I’m so happy he’s in my life! He’s a cutie, and I can’t imagine not having found him.
-                </p>
-            </div>
+                <img id="successStoryPageImage" src={cat} alt="Cat"/> 
+                <div id="successStoryPageText">
+                    <h1 id="successStoryPageHeader"> 
+                        Name: Ollie
+                        <br />Age: 3 years
+                        <br />Breed: American Shorthair
+                        <br />Adopted: 10/24/2019
+                        <br />
+                        <br />
+                        A message from Ollie's owner, Bob:
+                    </h1>
+                    <p> 
+                        Ollie has been a real friend! Coming home from a crazy day at work, I get to look forward to this goofball greeting me with lots of purring and cuddles. I found Ollie through Purrfect Match. I couldn’t go to my local shelter without first knowing if I had a specific pet in mind, because I can’t be away from work for very long during business hours. Perrfect Match helped me find Ollie, and I’m so happy he’s in my life! He’s a cutie, and I can’t imagine not having found him.
+                    </p>
+                </div>
             </Modal>
-            
+
             </div>
         )
     }
