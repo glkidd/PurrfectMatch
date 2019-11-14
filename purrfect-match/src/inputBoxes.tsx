75423ea84a7d83044d551add5da93b5431cae7dd
Joon Lee <joonhlee100@gmail.com>
2019-11-14 01:51:23 -0800

    Added dropdown bar and styled to match prototype

--- purrfect-match/src/inputBoxes.tsx
+++ purrfect-match/src/inputBoxes.tsx
@@ -10,10 +10,30 @@ export class Input extends React.Component<inputProps, {}> {
     public render() {
         return (
         <div className="row">
-            {this.props.prompt} 
+            <div className="label">
+                {this.props.prompt} 
+            </div>
             <div>
                 <input type="text" name="fname" className="textBox"/>
             </div>
+        </div>
+        );
+    }
+}
+
+export class Select extends React.Component<inputProps, {}> {
+    
+    public render() {
+        return (
+        <div className="row">
+            <div className="searchLabel">
+                {this.props.prompt} 
+            </div>
+            <select name="Shelter Employee" style={{width:"150px", height:"1.5em"}}>
+                <option value="select"></option>
+                <option value="yes">Yes, I am!</option>
+                <option value="no">No, I am not.</option>
+            </select>
         </div>
         );
     }
@@ -24,7 +44,7 @@ export class Message extends React.Component<inputProps, {}> {
     public render() {
         return (
         <div className="row">
-            {this.props.prompt} 
+            {this.props.prompt}
             <div>
                 <textarea name="message" rows={20} cols={40} className="textBox message" placeholder = "Type your message here...">
                 </textarea>
@@ -35,10 +55,13 @@ export class Message extends React.Component<inputProps, {}> {
 }
 
 export class Send extends React.Component<{},{}> {
+    
+    handleSubmit(event : any) {
+    }
 
     public render() {
         return (
-            <button id="send"> Send</button>
+            <button onClick={this.handleSubmit} id="send"> Send </button>
         );
     }
 }
