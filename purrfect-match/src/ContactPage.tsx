b1fcd836c495aa1aa82a9a13e44edbaab8880c65
fuzzything44 <slimesftw@gmail.com>
2019-11-17 18:39:11 -0800

    Fix contact page to have validation + submit

--- purrfect-match/src/ContactPage.tsx
+++ purrfect-match/src/ContactPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './ContactPage.css';
 import { Input, Select, Message, Send } from './inputBoxes';
-
+import { Api } from "Api";
 interface ContactPageProps {
 };
 
 interface ContactPageState {
+    firstName: string | undefined;
+    lastName: string | undefined;
+    subject: string | undefined;
+    email: string | undefined;
+    shelterEmployee: "Yes" | "No" | "" | undefined;
+    message: string | undefined;
 };
 
 const OPTION_ARRAY = ["Yes", "No"];
@@ -14,51 +20,101 @@ export class ContactPage extends React.Component<ContactPageProps, ContactPageSt
 
     constructor(props : any) {
         super(props);
+
+        this.state = {
+            firstName: undefined,
+            lastName: undefined,
+            subject: undefined,
+            email: undefined,
+            shelterEmployee: undefined,
+            message: undefined
+        };
+    }
+
+    fillAndValidateField = (field: keyof ContactPageState): boolean => {
+        if (this.state[field] === undefined) {
+            this.setState({
+                [field]: ""
+            } as any);
+            return false;
+        } else {
+            return this.state[field] != "";
+        }
+    }
+
+    handleSubmit = ()  => {
+        let formValid = true;
+        // Validate all fields filled
+        for (let elem of ["firstName", "lastName", "subject", "email", "shelterEmployee", "message"]) {
+            if (!this.fillAndValidateField(elem as keyof ContactPageState)) {
+                formValid = false;
+            }
+        }
+        if (formValid) {
+            // @ts-ignore: Strict null checks, but we know they're non-null from the validation we just did
+            Api.submitContactInfo(this.state.firstName, this.state.lastName, this.state.subject, this.state.email, this.state.shelterEmployee, this.state.message);
+        }
+    }
+
+    checkField = (fieldVal: string | undefined, error: string): string | undefined => {
+        if (fieldVal == "") {
+            return error;
+        }
+        return undefined;
     }
 
     public render() {
         return (
-            <form>
-                <h2 className="header"> Contact Us! </h2>
-                <div className="float">
+            <div className="contactPage">
+                <h2 className="header">Contact Us!</h2>
+                <div className="contactBoxes">
                     <Input
-                        prompt="First Name:">
-                    </Input>
+                        prompt="First Name:"
+                        error={this.checkField(this.state.firstName, "Provide your first name.")}
+                        onchange={(newValue: string) => this.setState({ firstName: newValue })}
+                    />
+                    <Input
+                        prompt="Last Name:"
+                        error={this.checkField(this.state.lastName, "Provide your last name.")}
+                        onchange={(newValue: string) => this.setState({ lastName: newValue })}
+                    />
+                    <Input
+                        prompt="Subject:"
+                        error={this.checkField(this.state.subject, "Provide a subject.")}
+                        onchange={(newValue: string) => this.setState({ subject: newValue })}
+                    />
+                    <Input
+                        prompt="Email Address:"
+                        error={this.checkField(this.state.email, "Provide your email.")}
+                        onchange={(newValue: string) => this.setState({ email: newValue })}
+                    />
                 </div>
-                <div className="rightBoxes">
-                    <Input
-                        prompt="Last Name:">
-                    </Input>
-                </div>
-                <Input
-                    prompt="Subject:">
-                </Input>
-                <div className="float">
-                    <Input
-                        prompt="Email Address:">
-                    </Input>
+                <div className="contactBoxes">
                     <div className="contactText">
-                        <p> Are you a shelter and need help with your account?</p>
-                        <p> Are you a potential pet owner with comments or</p>
-                        <p> concerns? Send us a note here! </p>
-                        <p> Or you can reach us at purrfectmatch@gmail.com </p>
-                </div>
-                </div>
-                <div className="search">
+                        <p>
+                            Are you a shelter and need help with your account?<br />
+                            Are you a potential pet owner with comments or<br />
+                            concerns? Send us a note here! <br />
+                            Or you can reach us at purrfectmatch@gmail.com
+                        </p>
+                    </div>
                     <Select
                         prompt="Are you a shelter employee?"
-                        options= {OPTION_ARRAY}>
-                    </Select>
+                        options={OPTION_ARRAY}
+                        error={this.checkField(this.state.shelterEmployee, "Choose an option.")}
+                        onchange={(newValue: any) => this.setState({ shelterEmployee: newValue })}
+                    />
                 </div>
-                <div className="label">
-                    <p >Message: </p>
+                <div className="contactBoxes">
+                    <Message
+                        prompt="Message:"
+                        error={this.checkField(this.state.message, "Please write a message.")}
+                        onchange={(newValue: string) => this.setState({ message: newValue })}
+                    />
                 </div>
-                <Message
-                    prompt=" ">
-                </Message>
-                <Send>
-                </Send>
-            </form>
+                <Send onclick={this.handleSubmit} />
+            </div>
+
         );
     }
 }
