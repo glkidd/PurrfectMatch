6fe62f1411920b9e0df1d7000110ed8763f614de
fuzzything44 <slimesftw@gmail.com>
2020-01-22 14:59:47 -0800

    Add .travis.yml, eslint definitions, start fixing some linter errors

--- purrfect-match/src/CompactView.tsx
+++ purrfect-match/src/CompactView.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Modal } from "./Modal";
-import { SuccessStoryInfo } from "Definitions"
+import { SuccessStoryInfo } from "Definitions";
 import cat from "./pictures/cat5.jpg";
 
 interface compactViewProps {
     info: SuccessStoryInfo;
-};
+}
 
 interface compactViewState {
     modalOpen: boolean;
-};
+}
 
 export class CompactView extends React.Component<compactViewProps, compactViewState> {
    
@@ -24,23 +24,23 @@ export class CompactView extends React.Component<compactViewProps, compactViewSt
     public render() {
         return (
             <div>
-                <button className="compactView" onClick={() => { this.setState({ modalOpen: true }) }}>
-                    <img id="animalImage" src={cat} alt="Image of animal"/>
+                <button className="compactView" onClick={() => { this.setState({ modalOpen: true }); }}>
+                    <img id="animalImage" src={cat} alt="Image of animal" />
                     <h1 id="animalName">
                         {this.props.info.name}
                     </h1>
                     <p id="animalInfo">
                         Breed: {this.props.info.breed}
-                        <br/> Age: {this.props.info.age} 
-                        <br/> Gender: {this.props.info.gender}
+                        <br /> Age: {this.props.info.age}
+                        <br /> Gender: {this.props.info.gender}
                     </p>
                 </button>
-                
+
                 <Modal display={this.state.modalOpen} onClose={() => this.setState({ modalOpen: false })}>
-                    <img id="successStoryPageImage" src={cat} alt="Cat"/> 
+                    <img id="successStoryPageImage" src={cat} alt="Cat" />
                     <div id="successStoryPageText">
-                        <h1> 
-                            Name: {this.props.info.name} 
+                        <h1>
+                            Name: {this.props.info.name}
                             <br />Age: {this.props.info.age}
                             <br />Breed: {this.props.info.breed}
                             <br />Adopted: {this.props.info.adoptionDate}
@@ -48,13 +48,13 @@ export class CompactView extends React.Component<compactViewProps, compactViewSt
                             <br />
                             A message from {this.props.info.name}'s owner, {this.props.info.owner}:
                         </h1>
-                        <p> 
+                        <p>
                             {this.props.info.storyText}
                         </p>
                     </div>
                 </Modal>
             </div>
-        )
+        );
     }
 
 }
