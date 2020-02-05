beec1e172b012dfba2826e8e21a7e40918ac7b8f
fuzzything44 <tmccorma@calpoly.edu>
2020-02-05 14:30:24 -0800

    Set up selenium tests. Currently only works with Chrome on Windows, fixing this takes some setup for everyone (#97)

---
+++ Backend/purrfect-match-backend/src/test/java/TestClass.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+    
+    @Test
+    public void Test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("localhost:3000");
+        
+        // Navigate to contact page
+        driver.findElement(By.id("contactLink")).click();
+        
+        driver.findElement(By.id("firstName")).sendKeys("name");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {}
+    }
+    
+}
