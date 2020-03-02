2dd89e02f9a49385abc41e3fac6d4d4f49a6bae8
Tim Gorer <lghstim@gmail.com>
2020-03-02 14:39:55 -0800

    remove className 'feature-<number>'

--- purrfect-match/src/HomePage/Body.tsx
+++ purrfect-match/src/HomePage/Body.tsx
@@ -19,15 +19,15 @@ export default class Body extends React.Component<BodyProps, BodyState> {
     return (
       <div>
         <div className="body-wrapper">
-          <div className="feature-1">
+          <div>
             <h1 className="feature-header">1. Find your perfect pet!</h1>
             <p className="paragraph-header">We've partnered with hundreds of animal shelters to help you with the process.</p>
           </div>
-          <div className="feature-2">
+          <div>
             <h1 className="feature-header">2. Contact the shelter.</h1>
             <p className="paragraph-header">Each pet should have contact information for its respective shelter.</p>
           </div>
-          <div className="feature-3">
+          <div>
             <h1 className="feature-header">3. Pick up your companion.</h1>
             <p className="paragraph-header">Once you pick up your companion you can leave a success story and be featured on the site.</p>
           </div>
