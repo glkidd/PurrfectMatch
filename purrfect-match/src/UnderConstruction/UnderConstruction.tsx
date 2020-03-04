245d8b396821e3cb9ea8f042e39f09796e72dfd6
AJ Jiro <ajjiro@AJs-MacBook-Pro-2.local>
2020-03-03 18:08:20 -0800

    Two buttons

--- purrfect-match/src/UnderConstruction/UnderConstruction.tsx
+++ purrfect-match/src/UnderConstruction/UnderConstruction.tsx
@@ -9,16 +9,8 @@ function UnderConstruction() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Under Construction 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
