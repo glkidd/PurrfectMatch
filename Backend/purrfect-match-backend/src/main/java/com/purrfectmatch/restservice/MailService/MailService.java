560c4aa7462699d9bfbfa3f2b00c6c2952b1c7f7
AJ Jiro <ajjiro@AJs-MacBook-Pro-3.local>
2020-03-10 12:54:12 -0700

    ContactPage Backend

---
+++ Backend/purrfect-match-backend/src/main/java/com/purrfectmatch/restservice/MailService/MailService.java
@@ -0,0 +1,27 @@
+package com.purrfectmatch.restservice.MailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.purrfectmatch.restservice.ContactPageModel.ContactPage; 
+
+
+@Service
+public class MailService {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Autowired
+	public void sendEmail(final ContactPage user) throws MailException {
+		   SimpleMailMessage mail = new SimpleMailMessage();
+		   String info = user.toString();
+		   mail.setTo("purrfectmatch309@gmail.com");
+		   mail.setSubject(user.getSubject());
+		   mail.setText(info);
+			javaMailSender.send(mail);
+	}
+}
--- Backend/purrfect-match-backend/src/main/java/com/purrfectmatch/restservice/RestServiceApplication.java
+++ Backend/purrfect-match-backend/src/main/java/com/purrfectmatch/restservice/RestServiceApplication.java
@@ -2,15 +2,15 @@ package com.purrfectmatch.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Collections;
+
+
 
 @SpringBootApplication
 public class RestServiceApplication {
         public static void main(String... args) {
                 // Change port to run on here:
-                SpringApplication app = new SpringApplication(RestServiceApplication.class);
-                app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
-                app.run(args);
+                ConfigurableApplicationContext app = SpringApplication.run(RestServiceApplication.class, args);
         }
 }
