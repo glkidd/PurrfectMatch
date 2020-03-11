48aaf7ceac19a180c83b46a36ec981f4b0671c52
AJ Jiro <ajjiro@AJs-MacBook-Pro-3.local>
2020-03-11 10:25:29 -0700

    resolved 2 comments

--- Backend/purrfect-match-backend/src/main/java/com/purrfectmatch/restservice/MailService/MailService.java
+++ Backend/purrfect-match-backend/src/main/java/com/purrfectmatch/restservice/MailService/MailService.java
@@ -6,22 +6,21 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.purrfectmatch.restservice.ContactPageModel.ContactPage; 
-
+import com.purrfectmatch.restservice.ContactPageModel.ContactPage;
 
 @Service
 public class MailService {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
-	
+
 	@Autowired
 	public void sendEmail(final ContactPage user) throws MailException {
-		   SimpleMailMessage mail = new SimpleMailMessage();
-		   String info = user.toString();
-		   mail.setTo("purrfectmatch309@gmail.com");
-		   mail.setSubject(user.getSubject());
-		   mail.setText(info);
-			javaMailSender.send(mail);
+		SimpleMailMessage mail = new SimpleMailMessage();
+		String info = user.toString();
+		mail.setTo("purrfectmatch309@gmail.com");
+		mail.setSubject(user.getSubject());
+		mail.setText(info);
+		javaMailSender.send(mail);
 	}
 }
